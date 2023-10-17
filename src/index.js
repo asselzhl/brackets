aa9b5bafc4ebb16fe5265277b72627b9d7b4c55b
asselzhl <asselzhly@gmail.com>
2023-10-17 21:02:32 +0600

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let values = [];
+    let keys = [];
+    let j = 0;
+    bracketsConfig.forEach(element => {
+       keys[j] = element[0]; 
+       values[j] = element[1];
+       j++;
+    });
+    
+    let stack = [];
+    let top;
+    for (let i = 0; i < str.length; i++) {
+        let currentSymbol = str[i];
+
+        top = stack[stack.length - 1];
+        let ind = values.indexOf(currentSymbol);
+        if (ind > -1 && keys[ind] == top) {
+            stack.pop();
+            continue;
+        }
+
+        if (keys.indexOf(currentSymbol) > -1) {
+            stack.push(currentSymbol);
+        } else {
+            stack.push(currentSymbol);
+            break;
+        }
+    }
+    return stack.length == 0;
 }
